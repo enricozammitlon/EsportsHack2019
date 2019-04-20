7453a1782c84313c35eebcbe7fe502dbc72d54e0
mattly179 <49762988+mattly179@users.noreply.github.com>
2019-04-20 14:15:17 +0200

    Update allEvents.php

--- allEvents.php
+++ allEvents.php
@@ -5,21 +5,6 @@
 <section id="search" class="alt">
 	<form method="post" action="#">
 		<input type="text" name="query" id="query" placeholder="Search" />
-		<button id="organiser" onClick = 'updateMenu(this.id)'>Organiser</button>
-
-		<div class="col-sm-4">
-			<div class="form-group">
-				<span class="form-label">Sort by...</span>
-				<select class="form-control" name = "filterby">
-					<option>Sort by No. of spectators (asc)</option>
-					<option>Sort by No. of spectators (desc)</option>
-					<option>Sort by Location (asc)</option>
-					<option>Sort by Location (desc)</option>
-				</select>
-				<span class="select-arrow"></span>
-			</div>
-		</div>
-
 	</form>
 </section>
 
